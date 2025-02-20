b35e4c0e87e594b76ce497a27a3558babf672f06
ABISHEAK-R <rabisheakofficial@gmail.com>
2025-02-20 23:55:34 +0530

    yes commit

--- student_management/student_management/urls.py
+++ student_management/student_management/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+
 from django.contrib import admin
 from django.urls import path
+from assignments.views import upload_assignment  # Import view
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('upload/', upload_assignment, name='upload'),  # Add this line
 ]
+
